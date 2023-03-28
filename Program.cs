31283685c9485dd71c6ae1d2d5933eebe0d01f82
PC4-IFLAB <PC4-IFLAB>
2023-03-28 17:06:45 +0700

    initialize this repository with

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+public class SayaTubeVideo
+{
+    public string Username { get; }
+
+    private int id;
+    string title;
+    int playCount;
+    int TotalVideoPLay;
+    private List<SayaTubeVideo> uploadedVideos;
+    string sayaTubeUser;
+
+    public SayaTubeVideo(string a)
+    {
+        this.title = a;
+        Random r = new Random();
+        id = r.Next(10000, 99999);
+        this.playCount = 0;
+    }
+
+    public void increasePlayCount(int pt)
+    {
+        this.playCount += pt;
+    }
+
+    public void printVideosDetails()
+    {
+        Console.WriteLine("id           : " + this.id);
+        Console.WriteLine("title        : " + this.title);
+        Console.WriteLine("playCount   :  " + this.playCount);
+    }
+
+    public sayaTubeUser(string username)
+    {
+        Debug.Assert(username != null);
+        Debug.Assert(username.Length <= 100);
+        Username = username;
+        id = new Random().Next(10000, 100000);
+        uploadedVideos= new List<SayaTubeVideo>();
+
+        
+
+    }
+
+    public
+}
